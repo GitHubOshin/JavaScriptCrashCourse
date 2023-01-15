21c99f5851b1738cca9b28853bf05d640ff56e40
OSHIN <81525447+GitHubOshin@users.noreply.github.com>
2023-01-15 23:15:48 +0700

    Learned arithmetic operators

---
+++ arithmetic-operators.js
@@ -0,0 +1,13 @@
+/**
+ * * ARITHMETIC OPERATORS
+
+ *  * Addition          +
+ *  * Subtraction       -
+ *  * Multiplication    *
+ *  * Division          /
+ *  * Moduls            %
+ *  * Exponentiation    **
+ */
+
+let result = (10 % 2) * 1 + 2
+console.log(result)
